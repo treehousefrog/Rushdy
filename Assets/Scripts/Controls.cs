7ccc09e7da22e0e7d10773f9d3473969cead9c2a
treehousefrog <rushdy_worm@hotmail.com>
2016-05-19 16:23:40 +0100

    Upload

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Controls : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float movespeed;
+    public bool moveright;
+    public bool moveleft;
+    
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+
+        }
+
+        if (moveright)
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+        if (moveleft)
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+
+    }
+    public void goright ()
+    {
+        
+
+    }
+    public void goleft ()
+    {
+        rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+
+    }
+}
---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour
+{
+    private Controls player;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Controls>();
+    }
+
+    public void LeftArrow()
+    {
+        player.moveright = false;
+        player.moveleft = true;
+    }
+    public void RightArrow()
+    {
+        player.moveright = true;
+        player.moveleft = false;
+    }
+    public void ReleaseLeftArrow()
+    {
+        player.moveleft = false;
+    }
+    public void ReleaseRightArrow()
+    {
+        player.moveright = false;
+
+    }
+
+}
